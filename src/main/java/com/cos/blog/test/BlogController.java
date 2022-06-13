47ff068194830390fd30f01247ac3638ad796b8a
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-06-14 03:20:44 +0900

    http 요청 확인

--- src/main/java/com/cos/blog/test/BlogController.java
+++ src/main/java/com/cos/blog/test/BlogController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 //스프링이 com.cos.blog 패키지 이하를 스캔해서 모든 파일을 메모리에 new하는것은 아니고, 
 //특정 어노테이션이 붙어있는 클래스 파일들을 new해서(IoC ) 스프링 컨테이너에 관리해줍니다.
 @RestController						
-public class BlogControllerTest {
+public class BlogController {
     
 	@GetMapping("/test/hello")
 	public String hello() {
 		return "<h1>hello spring boot </h1>";
 	}
 }
+
