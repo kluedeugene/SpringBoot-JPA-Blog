e83bafaec7bba5173ed7a7d433874f1f6c372a2e
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-06-15 10:55:55 +0900

    lombok yml 설정

--- src/main/java/com/cos/blog/test/HttpController.java
+++ src/main/java/com/cos/blog/test/HttpController.java
@@ -14,10 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpController {
 
+    private static final String TAG = "HttpController: ";
+
+    @GetMapping("/http/lombok")
+    public String lombokTest() {
+        // Member m = new Member(1, "test", "1234", "email");
+        //builder를 이용해서 생성자를 자동으로 생성해줌
+        //lombok-builder를쓰면 장점은 추후에 데이터 삽입시 순서를 지키지않아도 된다. 순서 실수가 발생하지않는다.
+        Member m = Member.builder().id(1).username("test").password("1234").email("email").build();
+        System.out.println(TAG + "getter: " + m.getUsername());
+        m.setUsername("username");
+        System.out.println(TAG + "setter: " + m.getUsername());
+        return "lombokTest 완료";
+    }
     // 인터넷 브라우저 요청은 무조건 get요청밖에 할수없다.
     //  http://localhost:8080/http/get (select)
     @GetMapping("/http/get")
     public String getTest(Member m) {
+       
         return "get 요청: "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
     }
     
