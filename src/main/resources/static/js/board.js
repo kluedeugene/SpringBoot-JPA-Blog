55d5895123dd4c2ac19168bf107f2f7183b10f10
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-07-06 20:16:46 +0900

    글쓰기 완료

---
+++ src/main/resources/static/js/board.js
@@ -0,0 +1,37 @@
+let index = {
+	init: function () {
+		//on -> 파라미터 1 이벤트가 일어나면, 파라미터2를 실행.
+		$('#btn-save').on('click', () => {
+			//function(){} 대신에 사용하는 이뉴는 화살표함수로 this를 바인딩하기 위해서이다.
+			this.save();
+		});
+	},
+
+	save: function () {
+
+		let data = {
+			title: $('#title').val(),
+			content: $('#content').val(),
+		};
+
+		$.ajax({
+			//글쓰기 요청 post
+			type: 'POST',
+			url: '/api/board', //요청 Url
+			data: JSON.stringify(data),
+			contentType: 'application/json; charset=UTF-8',
+			dataType: 'json'
+		})
+			.done(function (response) {
+				alert('글쓰기가 완료되었습니다.');
+				console.log(response);
+				location.href = '/';
+			})
+			.fail(function (error) {
+				alert(JSON.stringify(error));
+			});
+	}
+
+};
+
+index.init();
