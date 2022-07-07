6da311ad397d13d34b62d5f269b74b38d02c2fae
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-07-07 19:12:29 +0900

    글목록 페이징

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -3,6 +3,9 @@ package com.cos.blog.controller;
 import com.cos.blog.service.BoardService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,20 +21,16 @@ public class BoardController {
 
 
     @GetMapping({ "", "/" }) // 아무것도 안적었을때, / 일때
-    public String index(Model model) {  //메인페이지로갈때 데이터를 가져가야한다.
+    public String index(Model model, @PageableDefault(size = 2,sort = "id",direction = Sort.Direction.DESC) Pageable pageable) {  //메인페이지로갈때 데이터를 가져가야한다.
 
         //model은 jsp에서 request정보라고 생각하면된다. 데이터를 파라미터로 담으면 들고 이동.-> index.jsp 의  ${boards}
-        model.addAttribute("boards",boardService.글목록());    // index.jsp로  boards 넘어간다
+        model.addAttribute("boards",boardService.글목록(pageable));    // index.jsp로  boards 넘어간다
         return "index";  //@ Controller 는 viewResolver가 작동하는데 해당 인덱스 페이지로 model.의 정보를 들고 이동한다.
         // 또한 뷰리졸버는 리턴값 앞뒤로 application.yml의 prefix,suffix를 붙인다.
 
     }
-    // 세션 데이터 접근
-    // public String index(@AuthenticationPrincipal PrincipalDetail principal) { //
-    // 세션 데이터 접근
-    // /WEB-INF/views/index.jsp
-    // System.out.println("로그인 사용자 아이디: " + principal.getUsername());
-    // System.out.println("로그인 사용자 비밀번호: " + principal.getPassword());
+
+
 
     @GetMapping("/board/saveForm")
     public String saveForm() {
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -7,7 +7,7 @@
 
 <%-- m-2 -> 마진값 --%>
 <%--request 정보가 넘어올때 jstl에서 boards를 받아주는 식 ${} --%>
-    <c:forEach var="board" items="${boards}">
+    <c:forEach var="board" items="${boards.content}">
         <div class="card m-2" >
             <div class="card-body">
                 <h4 class="card-title">${board.title}</h4>
@@ -17,6 +17,26 @@
         </div>
 
     </c:forEach>
+
+    <ul class="pagination justify-content-center">
+    <c:choose>
+        <c:when test="${boards.first}">
+            <li class="page-item disabled "><a class="page-link" href="?page=${boards.number-1}">Previous</a></li>
+        </c:when>
+        <c:otherwise>
+            <li class="page-item "><a class="page-link" href="?page=${boards.number-1}">Previous</a></li>
+        </c:otherwise>
+    </c:choose>
+
+    <c:choose>
+        <c:when test="${boards.last}">
+            <li class="page-item disabled"><a class="page-link" href="?page=${boards.number+1}">Next</a></li>
+        </c:when>
+        <c:otherwise>
+            <li class="page-item"><a class="page-link" href="?page=${boards.number+1}">Next</a></li>
+        </c:otherwise>
+    </c:choose>
+    </ul>
 
 
 </div>
